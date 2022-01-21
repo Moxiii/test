cf3fa05dd8cf03c7db091b33fc060878cf4b81e2
Moxiii <dpower632@gmail.com>
2022-01-21 12:27:07 +0100

    a

---
+++ engine.py
@@ -0,0 +1,17 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+import hashlib
+from cryptography.fernet import Fernet
+Pass = Fernet.generate_key()
+key = hashlib.sha256(Pass).digest()
+mode = AES.MODE_CBC
+saut = "\n"
+print("clé:" , Pass)
+print("Clé hasher : " ,key)
+IV = Random.new().read(AES.block_size)
+print("LE FAMEUX IV",IV)
+cipher = AES.new(key,mode)
+with open('FILE/PLAIN/test.txt' , 'rb') as file:
+    file.read()
+message = "message to encode"
+print(len(message), message)
